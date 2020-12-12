8d67ea78c9ed73078409da316b4f54cdd4ba985f
Tom <tjpalmer@pop-os.localdomain>
2020-12-12 13:05:07 -0700

    Tweak pascal casing

--- semi.pas
+++ semi.pas
@@ -4,14 +4,13 @@ type
   TVector = array of Real;
   TMatrix = array of TVector;
   TSprite = record
-    kind: String;
-    pos: TVector;
+    Kind: String;
+    Pos: TVector;
   end;
 
 var
-  sprite: TSprite = (kind: 'robot'; pos: (1, 1));
+  sprite: TSprite = (Kind: 'robot'; Pos: (1, 1));
   viewPos: TVector;
-  // transform: Array[1..2, 1..2] of Real = ((1, 2), (3, 4));
 
 function MatVecMul(mat: TMatrix; vec: TVector): TVector;
 var i, j: Integer;
@@ -30,13 +29,13 @@ function Rot(frac: real): TMatrix;
 var rad: Real;
 begin
   rad := Pi * frac;
-  Rot := TMatrix.create(
-    TVector.create(Cos(rad), -Sin(rad)),
-    TVector.create(Sin(rad), Cos(rad))
+  Rot := TMatrix.Create(
+    TVector.Create(Cos(rad), -Sin(rad)),
+    TVector.Create(Sin(rad), Cos(rad))
   )
 end;
 
 begin
-  viewPos := MatVecMul(Rot(0.5), sprite.pos);
-  WriteLn(sprite.kind, ' rotated to ', viewPos[0], viewPos[1])
+  viewPos := MatVecMul(Rot(0.5), sprite.Pos);
+  WriteLn(sprite.Kind, ' rotated to ', viewPos[0], viewPos[1])
 end.
